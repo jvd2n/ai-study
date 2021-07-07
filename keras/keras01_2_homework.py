5d8cca4920146f187073c78f371a14e7cb377f1e
muten-dev <juhnmayer@gmail.com>
2021-07-07 18:14:13 +0900

    tf.keras

---
+++ keras/keras01_2_homework.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+
+# x = [1, 2, 3, 4, 5]
+# y = [1, 2, 4, 3, 5]
+# x_pred = [6]
+# 완성한 후, 출력 결과 스크린 샷
+
+# 1. 데이터
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 4, 3, 5])
+
+# 2. 모델
+model = Sequential()
+model.add(Dense(1, input_dim=1))
+
+# 3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(x, y, epochs=20000, batch_size=1)
+
+# 4. 평가, 예측
+loss = model.evaluate(x, y)
+print('loss : ', loss)
+
+result = model.predict([6])
+print('x_pred [6]의 예측값 : ', result)

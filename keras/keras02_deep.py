757009bc4c95b52c048950b7e48fbda1a6216a01
muten-dev <juhnmayer@gmail.com>
2021-07-08 15:35:34 +0900

    210708

--- keras/keras02_deep.py
+++ keras/keras02_deep.py
@@ -13,14 +13,19 @@ y = np.array([1, 2, 4, 3, 5])
 
 # 2. 모델
 model = Sequential()
-model.add(Dense(1, input_dim=1))
+model.add(Dense(5, input_dim=1))
+model.add(Dense(3))
+model.add(Dense(2))
+model.add(Dense(7))
+model.add(Dense(4))
+model.add(Dense(1))
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(x, y, epochs=1000, batch_size=1)
+model.fit(x, y, epochs=3000, batch_size=1)
 
-# 4. 평가, 예측c
+# 4. 평가, 예측
 loss = model.evaluate(x, y)
 ic('loss : ', loss)
 
---
+++ keras/keras03.py
@@ -0,0 +1,51 @@
+from icecream import ic
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+# 양 끝의 [] 는 제외하고 셈
+# 1 : [1, 2, 3]                   >> []를 제거하면 스칼라 3개의 값을 가진 벡터 1개가 된다. (3, )
+# 2 : [[1, 2, 3]]                 >> 1행 3열
+# 3 : [[1, 2], [3, 4], [5. 6]]    >> 3행 2열
+# 4 : [[[1, 2, 3], [4, 5, 6]]]    >> 1면 2행 3열
+# 5 : [[[1, 2], [3, 4], [5, 6]]]  >> 1면 3행 2열
+# 6 : [[[1], [2]], [[3], [4]]]    >> 2면 2행 1열
+
+# 행 무시, 열 우선
+
+# 1. 데이터
+x = np.array([
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+  [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.5, 1.4, 1.3]
+  ]) # 2행 10열
+y = np.array([11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+
+# ic(x.shape)
+# ic(x1)
+x = np.transpose(x) # 행의 개수가 서로 동일해야 하므로
+# ic(x.shape) # (10, 2)
+# ic(y.shape) # (10,)
+# ic(x)
+# ic(x2.T)
+
+# 2. 모델
+model = Sequential()
+model.add(Dense(10, input_dim=2))
+model.add(Dense(6))
+model.add(Dense(4))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(1))
+
+# 3
+model.compile(loss='mse', optimizer='adam')
+
+model.fit(x, y, epochs=500, batch_size=1)
+
+# 4. 평가, 예측
+loss = model.evaluate(x, y)
+ic('loss : ', loss)
+
+x_pred = np.array([[10, 1.3]])
+ic(x_pred.shape) # (1, 2)
+result = model.predict(x_pred)
+ic('x_pred의 예측값 : ', result)

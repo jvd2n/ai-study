f5cf9bc77c197aaa4ae7d7a241235a5c39478ef0
muten-dev <juhnmayer@gmail.com>
2021-07-10 15:44:39 +0900

    commit by muten

--- keras/keras04_scatter.py
+++ keras/keras04_scatter.py
@@ -2,13 +2,13 @@ from icecream import ic
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+# import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-# 1. 데이터
 x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 y = np.array([1, 2, 4, 3, 5, 7, 9, 3, 8, 12])
 
-# 2. 모델
 model = Sequential()
 model.add(Dense(5, input_dim=1))
 model.add(Dense(3))
@@ -17,20 +17,21 @@ model.add(Dense(7))
 model.add(Dense(4))
 model.add(Dense(1))
 
-# 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=200, batch_size=1)
 
-model.fit(x, y, epochs=1000, batch_size=1)
-
-# 4. 평가, 예측
 loss = model.evaluate(x, y)
 ic('loss : ', loss)
 
-# result = model.predict([11])
-# ic('[11]의 예측값 : ', result)
+result = model.predict([11])
+ic('[11]의 예측값 : ', result)
 
+# y_predict = model.predict(x)
+ic(x, x.shape)
+x_list = x.tolist()
 y_predict = model.predict(x)
-ic(y_predict)
+ic(x_list)
+ic(y_predict, y_predict.shape)
 
 plt.scatter(x, y)
 plt.plot(x, y_predict, color='red')
--- keras/keras06_R2_2.py
+++ keras/keras06_R2_2.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
@@ -14,30 +15,18 @@ y = np.array([1, 2, 4, 3, 5])
 # 2. 모델
 model = Sequential()
 model.add(Dense(5, input_dim=1))
-model.add(Dense(1000))
-model.add(Dense(1000))
-model.add(Dense(80))
-model.add(Dense(90))
-model.add(Dense(60))
-model.add(Dense(80))
-model.add(Dense(500))
-model.add(Dense(70))
-model.add(Dense(40))
+model.add(Dense(25))
+model.add(Dense(50))
+model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(25))
-model.add(Dense(400))
-model.add(Dense(14))
-model.add(Dense(20))
-model.add(Dense(11))
-model.add(Dense(400))
-model.add(Dense(11))
-model.add(Dense(15))
+model.add(Dense(13))
 model.add(Dense(1))
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(x, y, epochs=1000, batch_size=1)
+model.fit(x, y, epochs=500, batch_size=3000)
 
 # 4. 평가, 예측c
 loss = model.evaluate(x, y)
@@ -46,11 +35,9 @@ y_predict = model.predict(x)
 ic('x의 예측값 : ', y_predict)
 
 
-from sklearn.metrics import r2_score
-
 r2 = r2_score(y, y_predict)  # y_test와 y_predict값을 통해 결정계수를 계산
 ic('R2 스코어 : ', r2)
 
 
-#과제2
-#R2를 0.9 이상으로 올려라
+# 과제2
+# R2를 0.9 이상으로 올려라

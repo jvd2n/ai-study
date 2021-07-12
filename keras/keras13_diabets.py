0089c154691244be243f51ca71b666fa9dd4628e
muten-dev <juhnmayer@gmail.com>
2021-07-12 22:23:25 +0900

    r2r2

--- keras/keras13_diabets.py
+++ keras/keras13_diabets.py
@@ -22,25 +22,62 @@ ic(datasets.DESCR)
 ic(y[:30])
 ic(np.min(y), np.max(y))
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=9)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9, random_state=10)
 
 #2. 모델 구성
 # 모델링은 역삼각 노드, 다이아 노드, 모래시계 노드 등이 일반적이다.
+# optimizers=['adam']
+# activations=['elu', 'relu', 'linear']
+
+# r2score = []
+# opt = []
+# act = []
+# y_pred = []
+# from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+# early_stopping = EarlyStopping(monitor='val_loss', patience=60, mode='min')
+
+# model = Sequential()
+# for i in range(len(optimizers)):
+#     for j in range(len(activations)):
+#         model.add(Dense(256,activation=activations[j], input_shape=(10,)))
+#         model.add(Dense(128,activation=activations[j]))
+#         model.add(Dense(64,activation=activations[j]))
+#         model.add(Dense(32,activation=activations[j]))
+#         model.add(Dense(16,activation=activations[j]))
+#         model.add(Dense(1))
+#         model.compile(loss = 'mae', optimizer=optimizers[i])
+#         model.fit(x_train, y_train, epochs=500,
+#                   batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+        
+#         loss = model.evaluate(x_test, y_test)
+#         y_predict = model.predict(x_test)
+#         y_pred.append(y_predict)
+        
+#         r2 = r2_score(y_test, y_predict)
+#         r2score.append(r2)
+        
+#         opt.append(optimizers[i])
+#         act.append(activations[j])
+
+# print("loss : ", loss)
+# y_predict = model.predict(x_test)
+# index = r2score.index(max(r2score))
+# print("x value : ", y_pred[index])
+# print("best optimizer : ", opt[index],", best activation : ",act[index],", r2score : ", r2score[index])
+
 model = Sequential()
-model.add(Dense(512, input_dim=10, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(4, activation='relu'))
+model.add(Dense(256, input_shape=(10,),activation='selu')) 
+model.add(Dense(230,activation='selu'))
+model.add(Dense(190,activation='selu'))
+model.add(Dense(140,activation='selu'))
+model.add(Dense(130,activation='selu'))
 model.add(Dense(1))
 
-#3. 컴파일, 훈련
+# 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.015)
+model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.01, shuffle=True)
 
-#4. 평가, 예측
+# 4. 평가, 예측
 # mse, R2 -> R2 값을 1에 가깝게
 loss = model.evaluate(x_test, y_test)
 ic(loss)

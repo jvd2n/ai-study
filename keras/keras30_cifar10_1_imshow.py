6c9147ddc9c2cb8e8e3cf8baac28448dcf4cb5a3
muten-dev <juhnmayer@gmail.com>
2021-07-16 17:24:47 +0900

    keras~fashion_mnist~cifar

---
+++ keras/keras30_cifar10_1_imshow.py
@@ -0,0 +1,18 @@
+from icecream import ic
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import cifar10
+
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+ic(x_train.shape, y_train.shape)
+ic(x_test.shape, y_test.shape)
+# ic| x_train.shape: (60000, 28, 28), y_train.shape: (60000,)
+# ic| x_test.shape: (10000, 28, 28), y_test.shape: (10000,)
+
+ic(x_train[0])
+ic(y_train[0])
+
+plt.imshow(x_train[111])
+plt.show()
---
+++ keras/keras30_cifar10_2.py
@@ -0,0 +1,3 @@
+from tensorflow.keras.datasets import cifar10
+
+# image 32, 32, 3

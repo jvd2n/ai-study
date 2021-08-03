dcc3bb5e48524a034106e6679017676ce6f9e128
muten-dev <juhnmayer@gmail.com>
2021-08-03 23:24:32 +0900

    210803

--- PROJ_your_guitar/your_guitar_chord_1_save_npy.py
+++ PROJ_your_guitar/your_guitar_chord_1_save_npy.py
@@ -42,9 +42,10 @@ valid_gen = train_idg.flow_from_directory(
 
 test_gen = test_idg.flow_from_directory(
     '../data/guitar_chord_pred',
-    batch_size=15,
+    batch_size=14,
     target_size=(150, 150),
     class_mode='categorical',
+    shuffle=False,
 )
 # Found 14 images belonging to 14 classes.
 
@@ -57,8 +58,8 @@ ic(test_gen[0][1].shape)   # (14, 14)
 
 # ic(train_gen.shape)
 # ic(valid_gen.shape)
-
-augment_size = 20000 - train_gen[0][0].shape[0]
+'''
+augment_size = 50000 - train_gen[0][0].shape[0]
 
 randidx = np.random.randint(train_gen[0][0].shape[0], size=augment_size)
 ic(train_gen[0][0].shape[0])     # 60000
@@ -86,11 +87,12 @@ x_train = np.concatenate((x_train, x_augmented))
 y_train = np.concatenate((train_gen[0][1], y_augmented))
 
 ic(x_train.shape, y_train.shape)    # (100000, 28, 28, 1), (100000,)
+'''
 
-# np.save('./_save/_npy/ygc_train_x.npy', arr=train_gen[0][0])
-np.save('./_save/_npy/ygc_train_x.npy', arr=x_train)
-# np.save('./_save/_npy/ygc_train_y.npy', arr=train_gen[0][1])
-np.save('./_save/_npy/ygc_train_y.npy', arr=y_train)
+np.save('./_save/_npy/ygc_train_x.npy', arr=train_gen[0][0])
+# np.save('./_save/_npy/ygc_train_x.npy', arr=x_train)
+np.save('./_save/_npy/ygc_train_y.npy', arr=train_gen[0][1])
+# np.save('./_save/_npy/ygc_train_y.npy', arr=y_train)
 np.save('./_save/_npy/ygc_valid_x.npy', arr=valid_gen[0][0])
 np.save('./_save/_npy/ygc_valid_y.npy', arr=valid_gen[0][1])
 np.save('./_save/_npy/ygc_test_x.npy', arr=test_gen[0][0])
--- PROJ_your_guitar/your_guitar_chord_2_load_npy.py
+++ PROJ_your_guitar/your_guitar_chord_2_load_npy.py
@@ -29,43 +29,132 @@ ic(y_train[:10])
 # ic(y_train[1])
 # ic(y_test[1])
 
+x_train = x_train.reshape(-1, x_train.shape[1] * x_train.shape[2] * x_train.shape[3])
+x_valid = x_valid.reshape(-1, x_valid.shape[1] * x_valid.shape[2] * x_valid.shape[3])
 
+from sklearn.preprocessing import RobustScaler
+scaler = RobustScaler()
+x_train = scaler.fit_transform(x_train)
+x_valid = scaler.transform(x_valid)
+ic(x_train)
+ic(x_valid)
 
+x_train = x_train.reshape(-1, 150, 150, 3)
+x_valid = x_valid.reshape(-1, 150, 150, 3)
+
+# import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import InputLayer, Conv2D, MaxPooling2D, Dropout, Flatten, Dense, GlobalAveragePooling2D
 
 model = Sequential()
 model.add(InputLayer(input_shape=(150, 150, 3)))
 model.add(Conv2D(16, (3, 3), (1, 1), 'same', activation='relu'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Conv2D(32, (3, 3), (1, 1), 'same', activation='relu'))
 model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(14, activation='softmax'))
 
+'''
+VGG16_MODEL = VGG16(
+    input_shape = (150, 150, 3),
+    include_top = False,
+    weights = 'imagenet',
+)
+VGG16_MODEL.trainable = False
+# flatten이 없음 ( globalaveragepooling으로 대체 )
+#  ==> 가중치가 필요없음
+global_average_layer = GlobalAveragePooling2D()
+# FFNN의 가중치는 학습됨
+prediction_layer = Dense(14, activation ='softmax')
+model = Sequential([
+    VGG16_MODEL,
+    global_average_layer,
+    prediction_layer
+])
+
+
+from tensorflow.keras.applications import EfficientNetB0
+# load the MobileNetV2 network, ensuring the head FC layer sets are
+# left off
+baseModel = EfficientNetB0(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(150, 150, 3)))
+    
+# construct the head of the model that will be placed on top of the
+# the base model
+headModel = baseModel.output
+# headModel = AveragePooling2D(pool_size=(3, 3))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+# headModel = Dense(5, activation="relu")(headModel)
+headModel = Dropout(0.8)(headModel)
+headModel = Dense(3, activation="softmax")(headModel)
+
+# # place the head FC model on top of the base model (this will become
+# # the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
 # model.summary()
+
+for layer in baseModel.layers:
+	layer.trainable = False
+    
+
+print("[INFO] compiling model…")
+# opt = Adam(lr=INIT_LR)
+model.compile(loss="sparse_categorical_crossentropy", optimizer='adam',
+	metrics=["acc"])
+'''
+
+model.summary()
+
+# run_opts = tf.RunOptions(report_tensor_allocations_upon_oom = True)
 
 model.compile(
     loss='categorical_crossentropy', 
     optimizer='adam',
     metrics=['acc'],
+    # options=run_opts,
 )
 
-from tensorflow.keras.callbacks import EarlyStopping
-es = EarlyStopping(monitor='val_loss', patience=3, mode='min', verbose=1)
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience=5, mode='min', verbose=1, restore_best_weights=True)
+
+#########################################################################
+import datetime
+date = datetime.datetime.now()
+date_time = date.strftime("%y%m%d_%H%M")
+
+filepath = './PROJ_your_guitar/_save/ModelCheckPoint/' + date_time + '/'
+filename = '.{epoch:04d}-{val_loss:.4f}.hdf5'
+modelpath = "".join([filepath, "YGC_", date_time, "_", filename])
+#########################################################################
+
+mcp = ModelCheckpoint(
+    monitor='val_loss', 
+    mode='min', 
+    verbose=1, 
+    save_best_only=True, 
+    filepath=modelpath
+)
+
+model.save(filepath + 'YGC_MCP.h5')
 
 start_time = time.time()
 hist = model.fit(
     x_train, y_train,
-    epochs=100,
-    steps_per_epoch=32,
-    validation_steps=4,
+    epochs=30,
+    verbose=2,
+    # batch_size=2,
+    steps_per_epoch=16,
     validation_data=(x_valid, y_valid),
-    # callbacks=[es],
+    validation_steps=4,
+    callbacks=[mcp],
 )
 end_time = time.time() - start_time
 

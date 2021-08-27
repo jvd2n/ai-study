69baa3192a5f0f3bfc4e3e7826f85985e0e850e7
muten-dev <juhnmayer@gmail.com>
2021-08-27 10:23:14 +0900

    210827

--- tf114/tf18_mnist2_mlp.py
+++ tf114/tf18_mnist2_mlp.py
@@ -33,28 +33,36 @@ x = tf.compat.v1.placeholder(tf.float32, shape=[None, 28*28])
 y = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
 
 # Hidden Layer
-w1 = tf.Variable(tf.random.normal([28*28, 128], stddev=0.1, name='weight1'))
-b1 = tf.Variable(tf.random.normal([1, 128], stddev=0.1, name='bias1'))
+w1 = tf.Variable(tf.random.normal([28*28, 256], stddev=0.1, name='weight1'))
+b1 = tf.Variable(tf.random.normal([1, 256], name='bias1'))
 # hyppthesis = x * w + b
 layer1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+# layer1 = tf.nn.dropout(layer1, keep_prob=0.5)
 
 # Hidden Layer
-w2 = tf.Variable(tf.random.normal([128, 96], stddev=0.1, name='weight2'))
-b2 = tf.Variable(tf.random.normal([1, 96], stddev=0.1, name='bias2'))
+w2 = tf.Variable(tf.random.normal([256, 128], stddev=0.1, name='weight2'))
+b2 = tf.Variable(tf.random.normal([1, 128], name='bias2'))
 # hyppthesis = x * w + b
-layer2 = tf.nn.relu(tf.matmul(layer1, w2) + b2)
+layer2 = tf.nn.sigmoid(tf.matmul(layer1, w2) + b2)
+# layer2 = tf.nn.dropout(layer2, keep_prob=0.5)
 
 # Hidden Layer
-w3 = tf.Variable(tf.random.normal([96, 48], stddev=0.1, name='weight3'))
-b3 = tf.Variable(tf.random.normal([1, 48], stddev=0.1, name='bias3'))
+w3 = tf.Variable(tf.random.normal([128, 64], stddev=0.1, name='weight3'))
+b3 = tf.Variable(tf.random.normal([1, 64], name='bias3'))
 # hyppthesis = x * w + b
-layer3 = tf.nn.relu(tf.matmul(layer2, w3) + b3)
+layer3 = tf.nn.sigmoid(tf.matmul(layer2, w3) + b3)
+
+# Hidden Layer
+w4 = tf.Variable(tf.random.normal([64, 32], stddev=0.1, name='weight4'))
+b4 = tf.Variable(tf.random.normal([1, 32], name='bias4'))
+# hyppthesis = x * w + b
+layer4 = tf.nn.sigmoid(tf.matmul(layer3, w4) + b4)
 
 # Output Layer
-w4 = tf.Variable(tf.random.normal([48, 10], stddev=0.1, name='weight4'))
-b4 = tf.Variable(tf.random.normal([1, 10], stddev=0.1, name='bias4'))
+w5 = tf.Variable(tf.random.normal([32, 10], name='weight5'))
+b5 = tf.Variable(tf.random.normal([10], name='bias5'))
 # hyppthesis = x * w + b
-hypothesis = tf.nn.softmax(tf.matmul(layer3, w4) + b4)
+hypothesis = tf.nn.softmax(tf.matmul(layer4, w5) + b5)
 
 # layers1 = tf.sigmoid(tf.matmul(x_train, w) + b)
 # layers2 = tf.nn.relu(tf.matmul(x_train, w) + b)
@@ -65,14 +73,14 @@ hypothesis = tf.nn.softmax(tf.matmul(layer3, w4) + b4)
 # loss = tf.reduce_mean(tf.square(hypothesis-y)) # mse
 # loss = -tf.reduce_mean(y*tf.math.log(hypothesis)+(1-y)*tf.math.log(1-hypothesis))  # binary_crossentropy
 loss = tf.reduce_mean(-tf.reduce_sum(y* tf.math.log(hypothesis), axis=1)) # categorical_crossentropy
-
+# loss = tf.math.reduce_sum(tf.math.multiply(y, -tf.math.log(hypothesis)), axis=-1)
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
 train = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 
 with tf.compat.v1.Session() as sess:
     sess.run(tf.compat.v1.global_variables_initializer())
 
-    for epochs in range(101):
+    for epochs in range(121):
         import time
         st = time.time()
         _, loss_val = sess.run([train, loss], feed_dict={x:x_train, y:y_train})
@@ -83,3 +91,4 @@ with tf.compat.v1.Session() as sess:
     y_pred = np.argmax(predicted, axis=1)
     y_test = np.argmax(y_test, axis=1)
     print('accuracy_score: ', accuracy_score(y_test, y_pred))
+    # accuracy_score:  0.9747

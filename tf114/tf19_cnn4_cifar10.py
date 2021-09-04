dea5eca09f085ba2a1e2a8a28507d74e3445f005
muten-dev <juhnmayer@gmail.com>
2021-09-04 23:56:41 +0900

    commit by muten

--- tf114/tf19_cnn4_cifar10.py
+++ tf114/tf19_cnn4_cifar10.py
@@ -1,5 +1,7 @@
 # 0.7 이상
 
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import cifar10
 import numpy as np
 import tensorflow as tf
 # from keras.models import Sequential
@@ -13,17 +15,16 @@ print(tf.__version__)           # 1.14.0 -> 2.4.0
 
 tf.compat.v1.set_random_seed(66)
 
-#1 Data
+
+# 1 Data
 # from keras.datasets import mnist
-from tensorflow.keras.datasets import cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 # from keras.utils import to_categorical
-from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-print(x_train.shape, y_train.shape) # (50000, 32, 32, 3) (50000, 10)
+print(x_train.shape, y_train.shape)  # (50000, 32, 32, 3) (50000, 10)
 print(x_test.shape, y_test.shape)   # (10000, 32, 32, 3) (10000, 10)
 
 x_train = x_train.reshape(-1, 32, 32, 3).astype('float32')/255
@@ -37,15 +38,16 @@ total_batch = int(len(x_train)/batch_size)
 x = tf.compat.v1.placeholder(tf.float32, [None, 32, 32, 3])
 y = tf.compat.v1.placeholder(tf.float32, [None, 10])
 
-#2 Modelng
+# 2 Modelng
 # layer1
 w1 = tf.compat.v1.get_variable(name='w1', shape=[3, 3, 3, 32])
-                            # kernel_size, input, output
+# kernel_size, input, output
 print(w1)   # (3, 3, 1, 32)
 layer1 = tf.nn.conv2d(x, w1, strides=[1, 1, 1, 1], padding='SAME')
-print(layer1)   #  VALID -> (?, 26, 26, 32) // SAME -> (?, 28, 28, 32)
+print(layer1)  # VALID -> (?, 26, 26, 32) // SAME -> (?, 28, 28, 32)
 layer1 = tf.nn.relu(layer1)
-l1_maxpool = tf.nn.max_pool(layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+l1_maxpool = tf.nn.max_pool(layer1, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='SAME')
 
 
 # model = Sequential()
@@ -61,7 +63,8 @@ print(l1_maxpool)   # (?, 14, 14, 32)
 w2 = tf.compat.v1.get_variable('w2', shape=[3, 3, 32, 64])
 layer2 = tf.nn.conv2d(l1_maxpool, w2, strides=[1, 1, 1, 1], padding='SAME')
 layer2 = tf.nn.relu(layer2)
-l2_maxpool = tf.nn.max_pool(layer2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+l2_maxpool = tf.nn.max_pool(layer2, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='SAME')
 
 print(layer2)   # (?, 14, 14, 64)
 print(l2_maxpool)   # (?, 7, 7, 64)
@@ -70,26 +73,30 @@ print(l2_maxpool)   # (?, 7, 7, 64)
 w3 = tf.compat.v1.get_variable('w3', shape=[3, 3, 64, 128])
 layer3 = tf.nn.conv2d(l2_maxpool, w3, strides=[1, 1, 1, 1], padding='SAME')
 layer3 = tf.nn.relu(layer3)
-l3_maxpool = tf.nn.max_pool(layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+l3_maxpool = tf.nn.max_pool(layer3, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='SAME')
 
 print(layer3)   # (?, 7, 7, 128)
 print(l3_maxpool)   # (?, 4, 4, 128)
 
 # layer4
-w4 = tf.compat.v1.get_variable('w4', shape=[2, 2, 128, 64],)# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+w4 = tf.compat.v1.get_variable('w4', shape=[2, 2, 128, 64],)
 layer4 = tf.nn.conv2d(l3_maxpool, w4, strides=[1, 1, 1, 1], padding='VALID')
 layer4 = tf.nn.relu(layer4)
-l4_maxpool = tf.nn.max_pool(layer4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+l4_maxpool = tf.nn.max_pool(layer4, ksize=[1, 2, 2, 1], strides=[
+                            1, 2, 2, 1], padding='SAME')
 
 print(layer4)   # (?, 3, 3, 64)
 print(l4_maxpool)   # (?, 2, 2, 64)
 
 # Flatten
 layer_flat = tf.reshape(l4_maxpool, [-1, 2*2*64])
-print('Flatten(): ', layer_flat) # (?, 256)
+print('Flatten(): ', layer_flat)  # (?, 256)
 
 # layer5 DNN
-w5 = tf.compat.v1.get_variable('w5', shape=[2*2*64, 64],)# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+w5 = tf.compat.v1.get_variable('w5', shape=[2*2*64, 64],)
 b5 = tf.Variable(tf.random.normal([64]), name='b1')
 layer5 = tf.matmul(layer_flat, w5) + b5
 layer5 = tf.nn.relu(layer5)
@@ -97,7 +104,8 @@ layer5 = tf.nn.relu(layer5)
 print(layer5)   # (?, 64)
 
 # layer6 DNN
-w6 = tf.compat.v1.get_variable('w6', shape=[64, 32],)# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+# initializer=tf.compat.v1.contrib.layers.xavier_initializer())
+w6 = tf.compat.v1.get_variable('w6', shape=[64, 32],)
 b6 = tf.Variable(tf.random.normal([32]), name='b2')
 layer6 = tf.matmul(layer5, w6) + b6
 layer6 = tf.nn.relu(layer6)
@@ -111,13 +119,14 @@ layer7 = tf.matmul(layer6, w7) + b7
 hypothesis = tf.nn.softmax(layer7)
 print(hypothesis)   # (?, 10)
 
-#3 Compile
+# 3 Compile
 # categorical crossentropy
 loss = tf.reduce_mean(-tf.reduce_sum(y * tf.math.log(hypothesis), axis=1))
 
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 # train = optimizer.minimize(cost)
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+optimizer = tf.compat.v1.train.AdamOptimizer(
+    learning_rate=learning_rate).minimize(loss)
 
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
@@ -129,24 +138,25 @@ sess.run(tf.compat.v1.global_variables_initializer())
 
 for epoch in range(training_epochs):
     avg_loss = 0
-    
+
     for i in range(total_batch):
         # 몇 번 돌까?
         start = i * batch_size
         end = start + batch_size
         batch_x, batch_y = x_train[start:end], y_train[start:end]
-        
-        feed_dict = {x:batch_x, y:batch_y}
+
+        feed_dict = {x: batch_x, y: batch_y}
 
         batch_loss, _ = sess.run([loss, optimizer], feed_dict=feed_dict)
 
         avg_loss += batch_loss/total_batch
-        
-    print('Epoch:', '%04d' %(epoch + 1), 'loss: {:.9f}'.format(avg_loss))
+
+    print('Epoch:', '%04d' % (epoch + 1), 'loss: {:.9f}'.format(avg_loss))
 
 print('Training Complete')
 
-prediction = tf.equal(tf.compat.v1.argmax(hypothesis, 1), tf.compat.v1.argmax(y, 1))
+prediction = tf.equal(tf.compat.v1.argmax(
+    hypothesis, 1), tf.compat.v1.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
-print('Acc: ', sess.run(accuracy, feed_dict={x:x_test, y:y_test}))
+print('Acc: ', sess.run(accuracy, feed_dict={x: x_test, y: y_test}))
 # Acc:  0.7211
